7be9cdbb7cc7a59ea01f525293a255e2591e0d0a
orion <orion@system76-pc.localdomain>
2020-12-05 19:35:11 +0000

    test build

--- deploy/prod.tf
+++ deploy/prod.tf
@@ -2,7 +2,6 @@
 module "s3_bucket" {
   source = "./s3_bucket"
   GC_PROJECT_ID = "${var.GC_PROJECT_ID}"
-  IMAGE   = "${var.IMAGE}"
 }
 
 module "cloudsql" {
@@ -13,6 +12,4 @@ module "cloudsql" {
 
 module "gcr_cloud_run" {
   source = "./gcr_cloud_run"
-  GC_PROJECT_ID = "${var.GC_PROJECT_ID}"
-  IMAGE   = "${var.IMAGE}"
 }
