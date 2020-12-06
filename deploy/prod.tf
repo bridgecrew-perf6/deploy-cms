f0d0bcd58216adf83eea9259bc3dc10c720594d6
orion <orion@system76-pc.localdomain>
2020-12-06 00:45:05 +0000

    test build

--- deploy/prod.tf
+++ deploy/prod.tf
@@ -52,42 +52,42 @@ data "google_secret_manager_secret_version" "database_instance_name" {
   version = "1"
 }
 
-data "google_secret_manager_secret_version" "database_name" {
-  secret  = "database_name"
-  version = "1"
-}
+#data "google_secret_manager_secret_version" "database_name" {
+#  secret  = "database_name"
+#  version = "1"
+#}
 
-data "google_secret_manager_secret_version" "database_user" {
-  secret  = "database_user"
-  version = "1"
-}
+#data "google_secret_manager_secret_version" "database_user" {
+#  secret  = "database_user"
+#  version = "1"
+#}
 
-data "google_secret_manager_secret_version" "database_password" {
-  secret  = "database_password"
-  version = "1"
-}
+#data "google_secret_manager_secret_version" "database_password" {
+#  secret  = "database_password"
+#  version = "1"
+#}
 
 ## Provisioning a Postgres Database
-resource "google_sql_database_instance" "myinstance" {
-  name = data.google_secret_manager_secret_version.database_instance_name.secret_data
-  region = var.region
-  database_version = "POSTGRES_11"
+#resource "google_sql_database_instance" "myinstance" {
+#  name = data.google_secret_manager_secret_version.database_instance_name.secret_data
+#  region = var.region
+#  database_version = "POSTGRES_11"
 
-  settings {
-    tier = "db-f1-micro"
-  }
-}
+#  settings {
+#    tier = "db-f1-micro"
+#  }
+#}
 
-resource "google_sql_user" "users" {
-  name     = data.google_secret_manager_secret_version.database_user.secret_data
-  instance = google_sql_database_instance.myinstance.name
-  password = data.google_secret_manager_secret_version.database_password.secret_data
-}
+#resource "google_sql_user" "users" {
+#  name     = data.google_secret_manager_secret_version.database_user.secret_data
+#  instance = google_sql_database_instance.myinstance.name
+#  password = data.google_secret_manager_secret_version.database_password.secret_data
+#}
 
-resource "google_sql_database" "database" {
-  name     = data.google_secret_manager_secret_version.database_name.secret_data
-  instance = google_sql_database_instance.myinstance.name
-}
+#resource "google_sql_database" "database" {
+#  name     = data.google_secret_manager_secret_version.database_name.secret_data
+#  instance = google_sql_database_instance.myinstance.name
+#}
 
 
 ## Deploy image to Cloud Run
